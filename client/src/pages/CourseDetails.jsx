076b7f041b26f43cf598cc8fc633739fd1f02906
Animesh Bhardwaj <animeshbhardwajofficial@gmail.com>
2026-06-23 19:46:22 +0530

    Fetch course details from backend

--- client/src/pages/CourseDetails.jsx
+++ client/src/pages/CourseDetails.jsx
@@ -1,7 +1,6 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-
-import courses from "../data/courses";
+import axios from "axios";
 
 import {
     EnrollmentContext,
@@ -12,6 +11,12 @@ function CourseDetails() {
 
     const navigate = useNavigate();
 
+    const [course, setCourse] =
+        useState(null);
+
+    const [loading, setLoading] =
+        useState(true);
+
     const {
         enrollCourse,
         enrolledCourses,
@@ -19,13 +24,38 @@ function CourseDetails() {
         EnrollmentContext
     );
 
-    const course = courses.find(
-        (course) =>
-            course.id === Number(id)
-    );
+    useEffect(() => {
+        const fetchCourse =
+            async () => {
+                try {
+                    const response =
+                        await axios.get(
+                            `http://localhost:5000/api/courses/${id}`
+                        );
+
+                    setCourse(
+                        response.data.data
+                    );
+                } catch (error) {
+                    console.log(error);
+                } finally {
+                    setLoading(false);
+                }
+            };
+
+        fetchCourse();
+    }, [id]);
+
+    if (loading) {
+        return <h1>Loading...</h1>;
+    }
 
     if (!course) {
-        return <h1>Course Not Found</h1>;
+        return (
+            <h1>
+                Course Not Found
+            </h1>
+        );
     }
 
     const isEnrolled =
